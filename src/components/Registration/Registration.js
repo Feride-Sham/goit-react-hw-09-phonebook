34f20fa74d73bcba2c147908e07f0e2c7ff5dbf0
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-04-23 01:20:35 +0300

    refactor: transform Registration ti hooks

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,81 +1,80 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { authOperations } from "../../redux/auth";
 import s from "./Registration.module.css";
 
-class Registration extends Component {
-  state = {
-    name: "",
-    email: "",
-    password: "",
-  };
+export default function Registration() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  handleSubmit = (ev) => {
+  const handleSubmit = (ev) => {
     ev.preventDefault();
 
-    this.props.onRegister(this.state);
+    dispatch(authOperations.register({ name, email, password }));
 
-    this.setState({ name: "", email: "", password: "" });
+    setName("");
+    setEmail("");
+    setPassword("");
   };
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        console.error("Ooops");
+    }
   };
 
-  render() {
-    const { name, email, password } = this.state;
-    return (
-      <div>
-        <h1>Registration page</h1>
-        <form
-          onSubmit={this.handleSubmit}
-          autoComplete="off"
-          className={s.formBox}
-        >
-          <label className={s.formLabel}>
-            <span className={s.formSpan}>Name</span>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              className={s.formInput}
-            />
-          </label>
-          <label className={s.formLabel}>
-            <span className={s.formSpan}>Email</span>
+  return (
+    <div>
+      <h1>Registration page</h1>
+      <form onSubmit={handleSubmit} autoComplete="off" className={s.formBox}>
+        <label className={s.formLabel}>
+          <span className={s.formSpan}>Name</span>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            className={s.formInput}
+          />
+        </label>
+        <label className={s.formLabel}>
+          <span className={s.formSpan}>Email</span>
 
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-              className={s.formInput}
-            />
-          </label>
-          <label className={s.formLabel}>
-            <span className={s.formSpan}>Password</span>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            className={s.formInput}
+          />
+        </label>
+        <label className={s.formLabel}>
+          <span className={s.formSpan}>Password</span>
 
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-              className={s.formInput}
-            />
-          </label>
-          <button type="submit" className={s.formBtn}>
-            Register
-          </button>
-        </form>
-      </div>
-    );
-  }
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            className={s.formInput}
+          />
+        </label>
+        <button type="submit" className={s.formBtn}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
-
-// сокращенная запись mapDispatchToProps
-const mapDispatchToProps = {
-  onRegister: authOperations.register,
-};
-
-export default connect(null, mapDispatchToProps)(Registration);
