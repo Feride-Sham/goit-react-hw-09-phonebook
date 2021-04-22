9081376a2ee0dd6b59d0ab26f8a1dde168888457
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-04-22 15:02:36 +0300

    refactor: transfer UserMenu to hooks

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { authSelectors, authOperations } from "../../redux/auth";
 import s from "./UserMenu.module.css";
 import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
@@ -11,10 +11,16 @@ const style = {
   },
 };
 
-const UserMenu = ({ avatar, name, onLogout }) => {
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUserName);
+  const onLogout = useCallback(() => dispatch(authOperations.logout()), [
+    dispatch,
+  ]);
+
   return (
     <div className={s.userMenu}>
-      <img src={avatar} alt="" width="40" className={s.avatar} />
+      <img src={myAvatar} alt="" width="40" className={s.avatar} />
       <span className={s.span}>Hi, {name}</span>
       <ExitToAppOutlinedIcon
         onClick={onLogout}
@@ -23,15 +29,15 @@ const UserMenu = ({ avatar, name, onLogout }) => {
       />
     </div>
   );
-};
+}
 
-const mapStateToProps = (state) => ({
-  name: authSelectors.getUserName(state),
-  avatar: myAvatar,
-});
+// const mapStateToProps = (state) => ({
+//   name: authSelectors.getUserName(state),
+//   avatar: myAvatar,
+// });
 
-const mapDispatchToProps = {
-  onLogout: authOperations.logout,
-};
+// const mapDispatchToProps = {
+//   onLogout: authOperations.logout,
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
+// export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
